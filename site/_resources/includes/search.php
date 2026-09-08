2b9da12a71cb877653b2c9a0c7282dbf53c02d89
Maxwell Rowe <max@mackeycreativelab.com>
2026-09-08 16:22:55 -0700

    update container, gutter and max widths sitewide

--- site/_resources/includes/search.php
+++ site/_resources/includes/search.php
@@ -1,11 +1,12 @@
+<?php $searchInputId = 'lbcc-search-query-' . uniqid(); ?>
 <div class="d-flex flex-column flex-xl-row align-items-center gap-3 justify-content-between">
     <form class="site-search-form w-100" role="search" method="get">
-        <label class="visually-hidden" for="lbcc-search-query">Search the site</label>
+        <label class="visually-hidden" for="<?php echo lbcc_escape($searchInputId); ?>">Search the site</label>
         <div class="input-group">
             <span class="input-group-text bg-white border-end-0 rounded-start px-4 text-secondary">
                 <span class="fa-sharp fa-regular fa-magnifying-glass" aria-hidden="true"></span>
             </span>
-            <input class="form-control border-start-0 border-end-0 px-0" type="search" placeholder="Search LBCC">
+            <input id="<?php echo lbcc_escape($searchInputId); ?>" class="form-control border-start-0 border-end-0 px-0" type="search" placeholder="Search LBCC">
             <button class="btn btn-secondary border border-start-0 rounded-end px-4" type="button" aria-label="Search">
                 <span class="fa-sharp fa-regular fa-arrow-right" aria-hidden="true"></span>
             </button>
